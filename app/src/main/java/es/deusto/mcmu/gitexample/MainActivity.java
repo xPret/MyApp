c4cef3cc9c54e45c18a447204ea39a61de49f3d4
xPret <ivan.pretel@deusto.es>
2021-03-30 16:30:01 +0200

    [ADD] Task1-2-3 done

--- app/src/main/java/es/deusto/mcmu/gitexample/MainActivity.java
+++ app/src/main/java/es/deusto/mcmu/gitexample/MainActivity.java
@@ -2,13 +2,31 @@ package es.deusto.mcmu.gitexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = MainActivity.class.getName() + ".extra.MESSAGE";
+
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        String message = mMessageEditText.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
